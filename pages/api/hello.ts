e86356be9d5526fdfcacb33fc325dda5c6c6faef
Jenkin <jiancunge.com@gmail.com>
2022-05-24 14:26:26 +0800

    chore: add mock test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { mock, Random } from 'mockjs'
 
 type Data = {
   name: string
@@ -9,5 +10,13 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    mock({
+      'list|1-10': [{
+        'name|1': [Random.name(), Random.name(), Random.name()],
+        'id|+1': [Random.id(), Random.id(), Random.id()],
+        'address': Random.county(),
+      }]
+    })
+  )
 }
